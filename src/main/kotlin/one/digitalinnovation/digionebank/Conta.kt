dc0000d2e59e455b5516ef0eb3f02f8c3a2a9ff9
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-06-22 15:18:30 -0300

    Nova classe Conta com um construtor primário

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+//Construtor primário!
+class Conta (
+    val agencia: String,
+    val numConta: String,
+    val saldo: BigDecimal
+){
+    fun depositar(valor: BigDecimal){
+
+    }
+
+    fun sacar(valor: BigDecimal){
+
+    }
+}
