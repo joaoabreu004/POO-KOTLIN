10e896f14f22b3c3821ff847f07edc21838bc278
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-06-22 14:55:25 -0300

    Adicionando modificadores de acesso. (Set)

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,18 +1,15 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    var nome:String = "Joao";
-    var cpf:String = "559.010.390-92";
-
-    //Classe Interna (Inner Class)
-    inner class Endereco{
-        var rua:String = "Rua das Rosas"
-    }
+    var nome:String = "Joao"
+    private set
+    var cpf:String = "559.010.390-92"
+    private set
 }
 
 fun main(){
     val joao = Pessoa();
+//   joao.cpf = "534.434.433-39"
     println(joao.nome);
     println(joao.cpf);
-    println(joao.Endereco().rua)
 }
