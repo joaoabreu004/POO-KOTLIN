8207601b33fd696312e1b3b1469b0e588a7086ff
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-06-22 14:46:24 -0300

    Aula 2: Classe Interna

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,16 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome:String = "Joao";
     var cpf:String = "559.010.390-92";
+
+    //Classe Interna (Inner Class)
+    inner class Endereco{
+        var rua:String = "Rua das Rosas"
+    }
 }
 
- fun main(){
+fun main(){
     val joao = Pessoa();
     println(joao.nome);
     println(joao.cpf);
- }
+    println(joao.Endereco().rua)
+}
